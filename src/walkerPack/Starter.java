266347e3c191183d5b36da4932407cf5e3aeaedf
robz <robenkaraa29@gmail.com>
2021-05-15 23:21:38 +0200

    fixed merge with master

--- src/walkerPack/Starter.java
+++ src/walkerPack/Starter.java
@@ -10,26 +10,10 @@ public class Starter
 
     EquivalenceManager.initManager(); // handles all comparison by building a hashMap just once
 
-    //        SettingsManager manager = SettingsManager.load() ; // load settings into app
-    //
-    //        GraphicalInterface.currentManager = manager ; // bind manager
-    //        GraphicalInterface.main(args); // launch graphical interface
+    SettingsManager manager = SettingsManager.load() ; // load settings into app
 
-        SearchGroup group = SearchGroup.fromXML("test-files/FormattedSequences.xml");
+    GraphicalInterface.currentManager = manager ; // bind manager
+    GraphicalInterface.main(args); // launch graphical interface
 
-        System.out.println(group);
-
-        long nanoTime = group.rankUsingSimilarity( new Sequence("CCCC") , new JaccardCoefficient() );
-
-        System.out.println("It took " + nanoTime/1000 );
-//        System.out.println(group);
-
-        group.filterSelection( new KNNOperator(10 ));
-
-        nanoTime = group.rankUsingSimilarity( new Sequence("ACGCCUCCACGAGUGUCUU") , new SED() );
-
-
-        System.out.println( "It took " +  nanoTime/1000 );
-//        System.out.println(group);
     }
 }
